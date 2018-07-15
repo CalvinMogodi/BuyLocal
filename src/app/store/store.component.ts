9f5427f4346dfab0520b92b733c53af75f366ce0
CalvinMogodi <Calvin.Mogodi@eoh.co.za>
2018-07-15 15:35:46 +0200

    add store

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -9,26 +9,135 @@ import { StoreProvider } from '../providers/store';
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent {
-  public currentUser = {};
+  public currentUser: any;
   public storeForm: FormGroup;
-  public submitAttempt: boolean = false;
+  public productForm: FormGroup;
+  public submitStoreAttempt: boolean = false;
+  public submitProductAttempt: boolean = false;
   public showStoreError: boolean = false;
   public storeError = '';
+  public showProductError: boolean = false;
+  public productError = '';
   public storeHeader = 'Add Store';
+  public productHeader = 'Add Product';
+  public stores = [];
   public store = {
     name: '',   
     description: '',
-  }
+    userId: 0,
+    createdDate: '',
+  };
+  public product = {
+    name: '',   
+    description: '',
+    price: 0.00,
+    isOnSpecial: false,
+    discount: 0.00,
+    imageName: '',
+    storeId: '',
+    createdDate: '',
+  };
   constructor(public formBuilder: FormBuilder, public storeProvider: StoreProvider, public router: Router) {
      this.currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
       this.storeForm = formBuilder.group({
       name: [this.store.name, Validators.compose([Validators.required])],
       description: [this.store.description, Validators.compose([Validators.required])]
     });
+
+    this.productForm = formBuilder.group({
+      name: [this.product.name, Validators.compose([Validators.required])],
+      description: [this.product.description, Validators.compose([Validators.required])],
+      price: [this.product.price, Validators.compose([Validators.required])],
+      discount: [this.product.discount],
+    });
+
+     this.storeProvider.getStoreByUserId({userId: this.currentUser.Id}).subscribe((response: any) => {
+        this.stores = response;
+     });
   }
 
   addStore(){
+    this.submitStoreAttempt = true;
+    this.showStoreError = false;
+    this.storeError = '';
 
+    if (this.storeForm.valid) {
+      this.store.userId = this.currentUser.Id;
+      this.store.createdDate = new Date().toString();
+       this.storeProvider.addStore(this.store).subscribe((response: any) => {
+        if (response.result && response.errorMessage == null) {
+          let element: HTMLElement = document.getElementById('closeStoreModal') as HTMLElement;
+          element.click(); 
+          this.clear();
+        }else if(response.errorMessage != null){
+          this.showStoreError = true;
+          this.storeError = response.errorMessage;
+        }else{
+          this.showStoreError = true;
+          this.storeError = 'We are unable to add your store right now, Please try again later.';
+        }
+       })
+    }
+  }
+
+   updateStore(){
+    this.submitStoreAttempt = true;
+    this.showStoreError = false;
+    this.storeError = '';
+  }
+
+  addProduct(){
+    this.submitProductAttempt = true;
+    this.showProductError = false;
+    this.productError = '';
+    if (this.productForm.valid) {
+       this.storeProvider.addProduct(this.product).subscribe((response: any) => {
+        if (response.result && response.errorMessage == null) {
+          let element: HTMLElement = document.getElementById('closeProductModal') as HTMLElement;
+          element.click();
+          this.clear();
+        }else if(response.errorMessage != null){
+          this.showProductError = true;
+          this.productError = response.errorMessage;
+        }else{
+          this.showProductError = true;
+          this.productError = 'We are unable to add your product right now, Please try again later.';
+        }
+       })
+    }    
+  }
+
+   updateProduct(){
+    this.submitProductAttempt = true;
+    this.showProductError = false;
+    this.productError = '';
+  }
+
+  clear(){
+    this.submitStoreAttempt = false;
+    this.submitProductAttempt = false;
+    this.showStoreError = false;
+    this.storeError = '';
+    this.showProductError = false;
+    this.productError = '';
+    this.storeHeader = 'Add Store';
+    this.productHeader = 'Add Product';
+    this.store = {
+      name: '',   
+      description: '',
+      userId: 0,
+      createdDate: '',
+    };
+    this.product = {
+      name: '',   
+      description: '',
+      price: 0.00,
+      isOnSpecial: false,
+      discount: 0.00,
+      imageName: '',
+      storeId: '',
+      createdDate: '',
+    };
   }
   
 }
